79009b7e4e0e527cded26b7e5c9dc24a323ec864
Erin Houston <erinhouston@gmail.com>
2013-10-01 15:44:38 -0600

    Finally put this under git
    different version of the maybe monad every thing is now let rather then
    with and return.

---
+++ Safely/Maybe.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safely
+{
+    public static class Maybe
+    {
+        public static TInput If<TInput>(this TInput o, Func<TInput, bool> evaluator)
+            where TInput : class
+        {
+            return (o == null) ? null : (evaluator(o) ? o : null);
+        }
+
+        public static TInput Unless<TInput>(this TInput o, Func<TInput, bool> evaluator)
+               where TInput : class
+        {
+            return (o == null) ? null : evaluator(o) ? null : o;
+        }
+
+        public static TInput IfDo<TInput>(this TInput o, Func<TInput, bool> evaluator, Action<TInput> action)
+            where TInput : class
+        {
+            if (o == null) return null;
+            if (evaluator(o))
+            {
+                action(o);
+            }
+            return o;
+        }
+
+        public static TInput Do<TInput>(this TInput o, Action<TInput> action)
+            where TInput : class
+        {
+            if (o == null) return null;
+            action(o);
+            return o;
+        }
+
+        public static IEnumerable<TInput> Each<TInput>(this IEnumerable<TInput> o, Action<TInput> action)
+            where TInput : class
+        {
+            if (o == null) return null;
+            o.AsParallel().ForAll(action);
+            return o;
+        }
+
+        public static TInput Recover<TInput>(this TInput o, Func<TInput> evaluator)
+            where TInput : class
+        {
+            return o ?? evaluator();
+        }
+
+        public static TResult Let<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator, Func<TResult> failEvaluator = null)
+            where TInput : class
+        {
+            return (o == null) ? (failEvaluator != null ? failEvaluator() : default(TResult)) : evaluator(o);
+        }
+
+        public static TResult Let<TKey, TResult>(this IDictionary<TKey, TResult> o, TKey key, Func<TResult> failEvaluator = null)
+        {
+            return (o == null || !o.ContainsKey(key)) ? (failEvaluator != null ? failEvaluator() : default(TResult)) : o[key];
+        }
+    }
+}
